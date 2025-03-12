27a96b5587f4662532f6126dd00b070635625b18
Pablo Nobile <pablonobile99@gmail.com>
2025-03-12 00:25:21 -0300

    falla stock item

--- src/componentes/Item/Item.jsx
+++ src/componentes/Item/Item.jsx
@@ -13,24 +13,29 @@ const Item = ({id, nombre, stock, precio, img, idCat, zona, tel, detail}) => {
   }
 
   return (
-
-    <div className='item-box'>
-        <p>{stock}</p>
-        <div className='item-box-img'>
-          <img src={img} alt={nombre} className='item-img'/>
-        </div>
-        <h3 className='item-title'>{nombre}</h3>
-        <p className='item-txt'>{idCat}</p>
-        <div className='item-zona'>
-          <img src="../..//public/images/gpsIcon.png" alt="gps" className='item-gps-icon'/>
-          <p className='item-zona-txt'>{zona}</p>
-        </div>
-        <p className='item-txt'> {precio}</p>
-        <p className='item-txt'></p>
-        <Link to={`/item/${id}`} className='link item-button'>
-          <span className='item-txt-bot'>Ver Detalles</span>
-        </Link>
-    </div>
+    <>
+      {
+        stock > 0 ? (
+          <div className='item-box'>
+            <p>{stock}</p>
+            <div className='item-box-img'>
+              <img src={img} alt={nombre} className='item-img'/>
+            </div>
+            <h3 className='item-title'>{nombre}</h3>
+            <p className='item-txt'>{idCat}</p>
+            <div className='item-zona'>
+              <img src="../..//public/images/gpsIcon.png" alt="gps" className='item-gps-icon'/>
+              <p className='item-zona-txt'>{zona}</p>
+            </div>
+            <p className='item-txt'> {precio}</p>
+            <p className='item-txt'></p>
+            <Link to={`/item/${id}`} className='link item-button'>
+              <span className='item-txt-bot'>Ver Detalles</span>
+            </Link>
+          </div>
+        ) : ("")
+      }
+    </>
   )
 }
 
